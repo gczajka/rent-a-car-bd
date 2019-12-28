7ba3859a72bf4009fce66a147bd970e3961863bd
gczajka <gczajkajv@gmail.com>
2019-12-28 09:40:41 +0100

    test commit

---
+++ src/main/java/com/backend/rentacarbd/RentACarBdApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rentacarbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarBdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/rentacarbd/RentACarBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rentacarbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
